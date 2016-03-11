cc475cdb39cbf154625d48fc00286e07b64f5d45
henry van dusen <hgv2102@columbia.edu>
2016-03-11 01:54:44 -0500

    git test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <a href='advertisements'>ads</a>
 <a href='books'>books</a>
+aaaaa
 	<?php get_template_part( 'loop', 'index' ); ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
